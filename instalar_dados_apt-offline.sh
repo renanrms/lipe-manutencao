f9913ee7bdf62bf6fc6443c6e24c814512bfcf2e
Renan Passos <34728048+renanrms@users.noreply.github.com>
2018-09-17 15:59:53 -0200

    Adiciona o gparted ao script offline.

--- instalar_dados_apt-offline.sh
+++ instalar_dados_apt-offline.sh
@@ -4,7 +4,7 @@
 # Este script atualiza o sistema e instala os programas mais usados pelo LIpE. Deve ser executado em uma máquina com a mesma distribuição Linux dos computadores em que os dados de atualização e instalação foram obtidos.
 
 
-LISTA_DE_PACOTES="ntp adobe-flashplugin wine-stable default-jre icedtea-netx synaptic vlc firefox libreoffice gedit tuxmath tuxpaint tuxtype squeak-vm scratch jclic idle3 arduino vim gedit geany"
+LISTA_DE_PACOTES="ntp adobe-flashplugin wine-stable default-jre icedtea-netx synaptic gparted vlc firefox libreoffice gedit tuxmath tuxpaint tuxtype squeak-vm scratch jclic idle3 arduino vim gedit geany"
 sudo apt-offline install update-upgrade-install.zip
 sudo apt-get upgrade
 sudo apt-get install $LISTA_DE_PACOTES
